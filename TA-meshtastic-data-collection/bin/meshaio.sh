912e52c9bc84934641f0f207a0cbd4f900a43c55
Nick Zambo <nzambo@gmail.com>
2021-01-14 22:37:40 -0500

    updated to 0.1.1 on the TA to support /json/report endpoint data collection

--- TA-meshtastic-data-collection/bin/meshaio.sh
+++ TA-meshtastic-data-collection/bin/meshaio.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#export PYTHONPATH=/usr/local/lib/python3.6/site-packages:/usr/local/lib64/python3.6/site-packages
+export PYTHONPATH=/usr/local/lib/python3.6/site-packages:/usr/local/lib64/python3.6/site-packages
 
 pid=`ps -e -o pid,cmd | grep meshaio.py | grep $1 | awk '{print $1}'`
 
 if [ ! $pid ]; then
    rm -f $SPLUNK_HOME/etc/apps/TA-meshtastic-data-collection/bin/data/*-$1.log
-   /usr/bin/python3 $SPLUNK_HOME/etc/apps/TA-meshtastic-data-collection/bin/meshaio.py $1 $2 $3 $4 $5 $6
+   python3 $SPLUNK_HOME/etc/apps/TA-meshtastic-data-collection/bin/meshaio.py $1 $2 $3 $4 $5 $6
 fi
